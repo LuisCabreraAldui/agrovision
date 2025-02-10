8fc00de777c2cfba16ab820f5d7569dba9947597
Luis Cabrera Aldui <luis.cabrera.aldui@outlook.com>
2025-02-10 14:43:39 -0500

    update k3s nodes -v4

--- ot-fastapi/main.tf
+++ ot-fastapi/main.tf
@@ -1,29 +1,29 @@
 # Proveedor Kubernetes
 module "provider" {
-  source = "provider.tf"
+  source = "./module/provider.tf"
 }
 
 # Variables globales
 module "variables" {
-  source = "variables.tf"
+  source = "./module/variables.tf"
 }
 
 # Despliegue de la API
 module "api_deployment" {
-  source = "deployments.tf"
+  source = "./module/deployments.tf"
 }
 
 # Despliegue del Worker
 module "worker_deployment" {
-  source = "deployments.tf"
+  source = "./module/deployments.tf"
 }
 
 # Servicio para la API
 module "api_service" {
-  source = "services.tf"
+  source = "./module/services.tf"
 }
 
 # Autoscaling de la API
 module "api_autoscaler" {
-  source = "autoscalers.tf"
+  source = "./module/autoscalers.tf"
 }
